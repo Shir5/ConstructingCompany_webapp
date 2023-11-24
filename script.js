4eccdc8ecb015158c76ae1f641905fb7c74f271c
Shir5 <Vova0120_black@icloud.com>
2023-11-24 19:47:25 +0300

    scaling transit for menu, adding fg color as a var

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("about-section");
     const textElement = document.getElementById("about-us-paragraph");
-    const lines = textElement.innerHTML.split("<br>");
+    const lines = textElement.innerHTML.split("\n");
     lines.forEach((line, index) => {
     const span = document.createElement("span");
     span.innerHTML = line;
