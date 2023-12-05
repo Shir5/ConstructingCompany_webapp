f4fd485756dc1dd7d634968e41cc3147e02357b0
Shir5 <Vova0120_black@icloud.com>
2023-12-05 22:14:00 +0300

    Adding some responsiveness

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function slider(n) {
     }
     slides[slideIndex - 1].style.display = "block";
 }
-
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("about-section");
     const textElement = document.getElementById("about-us-paragraph");
@@ -35,16 +34,39 @@ document.addEventListener("DOMContentLoaded", function () {
         textElement.appendChild(span);
     });
 
-    const observer = new IntersectionObserver((entries) =>{
-        entries.forEach((entry)=>{
-            console.log(entry)
-            if(entry.isIntersecting){
-                entry.target.classList.add('show')
-            }else{
-                entry.target.classList.remove('show')
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
             }
-        })
-    })
-    const scrollSection = document.querySelectorAll('.hidden')
+        });
+    });
+
+    const scrollSection = document.querySelectorAll('.hidden');
     scrollSection.forEach((el) => observer.observe(el));
+
 });
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector("#closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+} else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    closeIcon.style.position = "fixed";
+    menuIcon.style.display = "none";
+}
+}
+
+hamburger.addEventListener("click", toggleMenu);
